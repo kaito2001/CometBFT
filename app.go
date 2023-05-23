f095030c78bbe99f2cd03578ad352c546488f99c
kaito2001 <kaito.ptit@gmail.com>
2023-05-23 17:33:17 +0700

    update logic execute data

---
+++ app.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bytes"
+	"log"
+
+	abcitypes "github.com/cometbft/cometbft/abci/types"
+	"github.com/dgraph-io/badger/v3"
+)
+
+type KVStoreApplication struct {
+	db           *badger.DB
+	onGoingBlock *badger.Txn
+}
+
+var _ abcitypes.Application = (*KVStoreApplication)(nil)
+
+func NewKVStoreApplication(db *badger.DB) *KVStoreApplication {
+	return &KVStoreApplication{db: db}
+}
+
+func (app *KVStoreApplication) Info(info abcitypes.RequestInfo) abcitypes.ResponseInfo {
+	return abcitypes.ResponseInfo{}
+}
+
+func (app *KVStoreApplication) Query(req abcitypes.RequestQuery) abcitypes.ResponseQuery {
+	resp := abcitypes.ResponseQuery{Key: req.Data}
+
+	dbErr := app.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(req.Data)
+		if err != nil {
+			if err != badger.ErrKeyNotFound {
+				return err
+			}
+			resp.Log = "key does not exist"
+			return nil
+		}
+
+		return item.Value(func(val []byte) error {
+			resp.Log = "exists"
+			resp.Value = val
+			return nil
+		})
+	})
+	if dbErr != nil {
+		log.Panicf("Error reading database, unable to execute query: %v", dbErr)
+	}
+	return resp
+}
+
+func (app *KVStoreApplication) InitChain(chain abcitypes.RequestInitChain) abcitypes.ResponseInitChain {
+	return abcitypes.ResponseInitChain{}
+}
+
+func (app *KVStoreApplication) PrepareProposal(proposal abcitypes.RequestPrepareProposal) abcitypes.ResponsePrepareProposal {
+	return abcitypes.ResponsePrepareProposal{}
+}
+
+func (app *KVStoreApplication) ProcessProposal(proposal abcitypes.RequestProcessProposal) abcitypes.ResponseProcessProposal {
+	return abcitypes.ResponseProcessProposal{}
+}
+
+func (app *KVStoreApplication) BeginBlock(req abcitypes.RequestBeginBlock) abcitypes.ResponseBeginBlock {
+	app.onGoingBlock = app.db.NewTransaction(true)
+	return abcitypes.ResponseBeginBlock{}
+}
+
+func (app *KVStoreApplication) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.ResponseDeliverTx {
+	if code := app.isValid(req.Tx); code != 0 {
+		return abcitypes.ResponseDeliverTx{Code: code}
+	}
+
+	parts := bytes.SplitN(req.Tx, []byte("="), 2)
+	key, value := parts[0], parts[1]
+
+	if err := app.onGoingBlock.Set(key, value); err != nil {
+		log.Panicf("Error writing to database, unable to execute tx: %v", err)
+	}
+
+	return abcitypes.ResponseDeliverTx{Code: 0}
+}
+
+func (app *KVStoreApplication) EndBlock(block abcitypes.RequestEndBlock) abcitypes.ResponseEndBlock {
+	return abcitypes.ResponseEndBlock{}
+}
+
+func (app *KVStoreApplication) Commit() abcitypes.ResponseCommit {
+	if err := app.onGoingBlock.Commit(); err != nil {
+		log.Panicf("Error writing to database, unable to commit block: %v", err)
+	}
+	return abcitypes.ResponseCommit{Data: []byte{}}
+}
+
+func (app *KVStoreApplication) ListSnapshots(snapshots abcitypes.RequestListSnapshots) abcitypes.ResponseListSnapshots {
+	return abcitypes.ResponseListSnapshots{}
+}
+
+func (app *KVStoreApplication) OfferSnapshot(snapshot abcitypes.RequestOfferSnapshot) abcitypes.ResponseOfferSnapshot {
+	return abcitypes.ResponseOfferSnapshot{}
+}
+
+func (app *KVStoreApplication) LoadSnapshotChunk(chunk abcitypes.RequestLoadSnapshotChunk) abcitypes.ResponseLoadSnapshotChunk {
+	return abcitypes.ResponseLoadSnapshotChunk{}
+}
+
+func (app *KVStoreApplication) ApplySnapshotChunk(chunk abcitypes.RequestApplySnapshotChunk) abcitypes.ResponseApplySnapshotChunk {
+	return abcitypes.ResponseApplySnapshotChunk{}
+}
+
+func (app *KVStoreApplication) isValid(tx []byte) uint32 {
+	// check format
+	parts := bytes.Split(tx, []byte("="))
+	if len(parts) != 2 {
+		return 1
+	}
+
+	return 0
+}
+
+func (app *KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
+	code := app.isValid(req.Tx)
+	return abcitypes.ResponseCheckTx{Code: code}
+}
